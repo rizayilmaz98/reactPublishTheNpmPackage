6ff114e25cc689adfd38544758c928ccf25ca857
Rıza Yılmaz <78048019+rizayilmaz98@users.noreply.github.com>
2023-04-16 02:53:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from 'react'
 import styles from './styles.module.css'
 
 export const Button = ({text,type}) => {
-  console.log(type);
   return <button className={styles[type]}>{text}</button>
 }
